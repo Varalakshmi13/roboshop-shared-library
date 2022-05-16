f44fe810937a2a81a42c602440b6d3477ea5165b
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-05-16 09:28:34 +0530

    PYTHON pipeline

---
+++ vars/python.groovy
@@ -0,0 +1,26 @@
+def linkChecks() {
+  sh ''' 
+    #we commented this because dev is goin =g to check this failure
+    #~/node_modules/jslint/bin/jslint.js server.js
+    pylink *.py
+    echo Lint Check for ${(COMPONENT)}
+ '''
+}
+
+def call() {
+  pipeline {
+    agent any
+
+    stages {
+       // For Each Commit
+      stage('Lint Checks') {
+        steps {
+          script {
+            linkChecks()
+            }
+          }
+       } 
+    }  // End of Stages
+  }
+
+}
