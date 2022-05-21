bc6be5b9c6f05e8c486e07087b1743ffb661a844
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-05-21 20:18:43 +0530

    maven scripted pipeline

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,7 +2,7 @@ def call() {
 
   node {
     git branch: 'main', url: "https://github.com/Varalakshmi13/${COMPONENT}"
-    env.APP_TYPE = "nodejs"
+    env.APP_TYPE = "maven"
     common.lintChecks()
     env.ARGS="-Dsonar.java.binaries=target/"
     common.sonarCheck()
