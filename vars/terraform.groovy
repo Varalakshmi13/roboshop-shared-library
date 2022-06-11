008174e0dea0832a3b7c9d0228e0003cca73b427
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-06-11 18:15:24 +0530

    terraform vpc

---
+++ vars/terraform.groovy
@@ -0,0 +1,25 @@
+def call() {
+    properties([
+        parameters([
+            choice(choices: ['dev\nprod'], description: "Pick Env", name: "ENV"),
+        ]),
+    ])
+
+    node {
+
+        sh 'rm -rf'
+        git branch: 'main', url: "https://github.com/Varalakshmi13/${REPONAME}"
+
+        stage('Terrafile INIT') {
+            sh 'terrafile -f env-${ENV}/Terrafile'
+        }
+
+        stage('Terraform init') {
+            sh 'terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars'
+        }
+
+        stage('Terraform Plan') {
+            sh 'terraform plan var-file -var-file=env-${ENV}/${ENV}.tfvars'
+        }
+    }
+}
