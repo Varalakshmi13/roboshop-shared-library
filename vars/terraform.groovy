ed5ac2033b682331f5785802853c4b7b3eccaf41
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-06-11 18:50:29 +0530

    terraform vpc

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -10,21 +10,21 @@ def call() {
           sh 'rm -rf'
           git branch: 'main', url: "https://github.com/Varalakshmi13/${REPONAME}"
 
-        stage('Terrafile INIT') {
-            sh 'terrafile -f env-${ENV}/Terrafile'
-        }
+          stage('Terrafile INIT') {
+              sh 'terrafile -f env-${ENV}/Terrafile'
+          }
 
-        stage('Terraform init') {
-            sh 'terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars'
-        }
+          stage('Terraform init') {
+              sh 'terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars'
+          }
 
-        stage('Terraform Plan') {
-            sh 'terraform plan -var-file=env-${ENV}/${ENV}.tfvars'
-        }
+          stage('Terraform Plan') {
+              sh 'terraform plan -var-file=env-${ENV}/${ENV}.tfvars'
+          }
 
-        stage('Terraform Apply') {
-            sh 'terraform apply -var-file=env-${ENV}/${ENV}.tfvars'
-        }
+          stage('Terraform Apply') {
+              sh 'terraform apply -var-file=env-${ENV}/${ENV}.tfvars'
+          }
         } 
 
     }
