687e43940d68f172e3cf50ab16740c0489780eff
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-28 11:15:32 +0530

    Updating roboshop-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -33,6 +33,7 @@ def call() {
           stage('Terraform Plan') {
               sh '''
                 cd ${TERRAFORM_DIR}
+                export TF_VAR_APP_VERSION=${APP_VERSION}
                 terraform plan -var-file=env-${ENV}/${ENV}.tfvars -var APP_VERSION=${APP_VERSION}
                 '''
           }
